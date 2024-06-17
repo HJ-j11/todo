fa28884c1556f4bdcf8a1b27525d6bcb9146dfb7
Hyeji <66746270+HJ-j11@users.noreply.github.com>
2024-06-17 13:18:56 +0900

    토큰 발급에 필요한 API, DTO 설정

---
+++ src/main/java/com/example/todo/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.todo.service;
+
+
+import com.example.todo.entity.Member;
+import com.example.todo.entity.error.CommonErrorCode;
+import com.example.todo.entity.exception.RestApiException;
+import com.example.todo.entity.jwt.CustomUserDetails;
+import com.example.todo.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberMapper memberMapper;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> member = memberMapper.findMemberByUsername(username);
+        if(member.isPresent()) {
+            return createUserDetails(member.get());
+        } else {
+            throw new RestApiException(CommonErrorCode.USER_NOT_FOUND);
+        }
+    }
+    private CustomUserDetails createUserDetails(Member member) {
+        return new CustomUserDetails(member.getId(), member.getUsername(), member.getPassword(), member.getUserRole().name());
+    }
+}
